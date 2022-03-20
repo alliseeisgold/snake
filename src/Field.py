c671766e1c559dd0365cb02acfb1c98b6d0045d5
Asror Mirzoev <coder2002a@gmail.com>
2022-03-21 01:16:21 +0300

    Added Field.py that has Field class. It has implementation of the methods of this class

---
+++ src/Field.py
@@ -0,0 +1,17 @@
+class Field:
+    def __init__(self, lengthX = 720,
+                 heightY = 480, 
+                 background = [0, 0, 0]): #default field size is 720x480
+        self.lengthX = lengthX
+        self.heightY = heightY
+        self.background = background
+
+    def getWindowLength(self):
+        return self.lengthX
+
+    def getWindowHeight(self):
+        return self.heightY
+
+    def getBackgroundColor(self):
+        return self.background
+
